1051d33df80d14e6fac02a349f899d655895e69d
Julius Haring <jh822405@ima-zlw-ifu.rwth-aachen.de>
2018-05-15 10:20:55 +0200

    added PlanControlelr

---
+++ NetzplanTool/PlanController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetzplanTool
+{
+    class PlanController
+    {
+        private List<List<Vorgang>> Pfade { get; set; }
+        private string Ueberschrift { get; set; }
+        private int Gesamtdauer { get {
+                int i = 0;
+                return i;
+            } }
+        private Repository Repo { get; set; }
+
+        public PlanController(string Ueberschrift, List<Vorgang> Vorgaenge)
+        {
+            Repo = new Repository();
+            Pfade = new List<List<Vorgang>>();
+
+            this.Ueberschrift = Ueberschrift;
+
+            KalkVorwaerts();
+            KalkRueckwaerts();
+            KalkPuffer();
+            KalkPfad();
+            CheckZusammenhang();
+        }
+
+        private void KalkVorwaerts()
+        {
+
+        }
+
+        private void KalkRueckwaerts()
+        {
+
+        }
+
+        private void KalkVorwaerts(Vorgang v)
+        {
+
+        }
+
+        private void KalkRueckwaerts(Vorgang v)
+        {
+
+        }
+
+        private void KalkPuffer()
+        {
+
+        }
+
+        private void KalkPfad()
+        {
+
+        }
+
+        private void CheckZusammenhang()
+        {
+
+        }
+
+        private void WriteFile(String name)
+        {
+
+        }
+    }
+}
